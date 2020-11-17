8b2a550e78ab79f255d9734800e90e3050e5e2a6
justintam5 <jt.tam514@gmail.com>
2020-11-16 21:55:49 -0500

    New method of calculating theta1-5
    Valid for negative values of (theta3+theta4+delta), still does not differentiate between possible solutions or return values that are always between -180 and 180. Does also not

--- Lab2_InverseQ.m
+++ Lab2_InverseQ.m
@@ -1,23 +1,24 @@
 clear
 %\\\\\\\\\\\\\\\\\\\\\\\\\\\\\Forward Model\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 %---------------------Theta Values---------------------------
-theta1 = 90;
-theta2 = -20;
-theta3 = -80;
-theta4 = 19;
-theta5 = 0;
+Theta=[90 -20 -80 19 0];
 delta = 90;
 %--------------------Calculate Q-------------------------------
-a = theta3+theta4+delta; %arbritrary constant for simplicity
-b = theta1+theta2;
-c = theta5;
-Q = [cosd(c)*cosd(b)*cosd(a)+sind(c)*sind(b) -sind(c)*cosd(b)*cosd(a)+cosd(c)*sind(b)...
-    cosd(b)*sind(a) cosd(b)*(110*sind(a)+60*cosd(theta3)+96)+98*cosd(theta1);
-    cosd(c)*sind(b)*cosd(a)-sind(c)*cosd(b) -sind(c)*sind(b)*cosd(a)-cosd(c)*cosd(b)...
-    sind(b)*sind(a) sind(b)*(110*sind(a)+60*cosd(theta3)+96)+98*sind(theta1);
-    cosd(theta5)*sind(a) -sind(theta5)*sind(a) -cosd(a) -110*cosd(a)+60*sind(theta3)+157;
+a = Theta(3)+Theta(4)+delta; %arbritrary constant for simplicity
+b = Theta(1)+Theta(2);
+c = Theta(5);
+Q = [cosd(c)*cosd(b)*cosd(a)+sind(c)*sind(b) -sind(c)*cosd(b)*cosd(a)+...
+    cosd(c)*sind(b) cosd(b)*sind(a) cosd(b)*(110*sind(a)+60*...
+    cosd(Theta(3))+96)+98*cosd(Theta(1));
+    cosd(c)*sind(b)*cosd(a)-sind(c)*cosd(b) -sind(c)*sind(b)*cosd(a)-...
+    cosd(c)*cosd(b) sind(b)*sind(a) sind(b)*(110*sind(a)+60*cosd(Theta(3))...
+    +96)+98*sind(Theta(1));
+    cosd(c)*sind(a) -sind(c)*sind(a) -cosd(a) -110*cosd(a)+...
+    60*sind(Theta(3))+157;
     0 0 0 1];
-%disp(Theta);
+disp('| Theta1F | Theta2F | Theta3F | Theta4F | Theta5F |');
+fprintf(['|    %d   |    %d   |    %d   |    %d   |    %d   |\n']...
+    ,Theta(1),Theta(2),Theta(3),Theta(4),Theta(5));
 %\\\\\\\\\\\\\\\\\\\\\\\\\\\\\Inverse Model\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 %---------------------End Effector Values----------------------------------
 %{
@@ -58,10 +59,14 @@ for i=1:8
     EE(i,8)=18840-120*Q(3,4); %B
     EE(i,9)=-(EE(i,6)^2)+192*EE(i,6)-(Q(3,4))^2+314*Q(3,4)-25365; %C
     EE(i,10)=EE(i,8)^2+(EE(i,7)+EE(i,9))*(EE(i,7)-EE(i,9)); %rt
+    %to calculate theta3 we first check to see if the correspoding term
+    %being squarooted is positive, and only proceed if that is the case
     if EE(i,10)>=0
         EE(i,3)=2*atan2d((-EE(i,8)+((-1)^k)*sqrt(EE(i,10))),...
             (-EE(i,7)-EE(i,9)));
     end
+    %Knowing theta1-3 we can calculate a singular theta4/5 pair for every
+    %solution calculated so far, and thus we end up with 8 solutions.
     %Where sind(theta1+theta2) != 90 Deg +-180 for theta4 calculated below:
     EE(i,4)=atan2d(Q(1,3)/cosd(EE(i,1)+EE(i,2)),(-Q(3,3)))-EE(i,3)-delta;
     EE(i,5)=atan2d(-Q(3,2)/(sind(EE(i,3)+EE(i,4)+delta)),...
@@ -69,5 +74,5 @@ for i=1:8
 end
 %
 %----------Final end effector location and angles--------------
-disp('| Theta1  | Theta2  | Theta3  | Theta4  | Theta5 |')
+disp('| Theta1I | Theta2I | Theta3I | Theta4I | Theta5I |');
 disp(EE(:,1:5));
