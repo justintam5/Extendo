56bdf44af6f85a69d9c1433447d8c34245761b40
Justin Welsh Tam <jt.tam514@gmail.com>
2020-11-17 01:24:53 -0500

    Updated to match the input notation of the inverse model

--- Lab1_Analytical_Q.m
+++ Lab1_Analytical_Q.m
@@ -1,27 +1,27 @@
-%---------------------Theta Values ---------------------------
-theta1 = -82;
-theta2 = -67;
-theta3 = -31;
-theta4 = -42;
-theta5 = -50;
+clear
+%\\\\\\\\\\\\\\\\\\\\\\\\\\\\\Forward Model\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+%---------------------Theta Values-----------------------------------------
+Theta=[0 0 0 0 0];
 delta = 90;
-%--------------------Calculate Q-------------------------------
-a = theta3+theta4+delta; %arbritrary constant for simplicity
-b = theta1+theta2;
-c = theta5;
-Q = [cosd(c)*cosd(b)*cosd(a)+sind(c)*sind(b) -sind(c)*cosd(b)*cosd(a)+cosd(c)*sind(b)...
-    cosd(b)*sind(a) cosd(b)*(110*sind(a)+60*cosd(theta3)+96)+98*cosd(theta1);
-    cosd(c)*sind(b)*cosd(a)-sind(c)*cosd(b) -sind(c)*sind(b)*cosd(a)-cosd(c)*cosd(b)...
-    sind(b)*sind(a) sind(b)*(110*sind(a)+60*cosd(theta3)+96)+98*sind(theta1);
-    cosd(theta5)*sind(a) -sind(theta5)*sind(a) -cosd(a) -110*cosd(a)+60*sind(theta3)+157;
+%--------------------Calculate Q-------------------------------------------
+a = Theta(3)+Theta(4)+delta; %arbritrary constant for simplicity
+b = Theta(1)+Theta(2);
+c = Theta(5);
+Q = [cosd(c)*cosd(b)*cosd(a)+sind(c)*sind(b) -sind(c)*cosd(b)*cosd(a)+...
+    cosd(c)*sind(b) cosd(b)*sind(a) cosd(b)*(110*sind(a)+60*...
+    cosd(Theta(3))+96)+98*cosd(Theta(1));
+    cosd(c)*sind(b)*cosd(a)-sind(c)*cosd(b) -sind(c)*sind(b)*cosd(a)-...
+    cosd(c)*cosd(b) sind(b)*sind(a) sind(b)*(110*sind(a)+60*...
+    cosd(Theta(3))+96)+98*sind(Theta(1));
+    cosd(c)*sind(a) -sind(c)*sind(a) -cosd(a) -110*cosd(a)+...
+    60*sind(Theta(3))+157;
     0 0 0 1];
-
-%----------Final end effector location and angles--------------
+%----------------------Final end effector location and angles--------------
 %
 thetaf = (atan2d(Q(2,1), Q(1,1)));
 phif = (atan2d((-Q(3,1)),(Q(1,1)*cosd(thetaf)+Q(2,1)*sind(thetaf))));
 psif = (atan2d(Q(3,2),Q(3,3)));
 %}
-%--------------------Display Q----------------------------------
-%display(Q);
+%-------------------------------Display Q----------------------------------
+display(Q);
 endeffec=[0.0393701*Q(1,4) 0.0393701*Q(2,4) 0.0393701*Q(3,4) thetaf phif psif];
