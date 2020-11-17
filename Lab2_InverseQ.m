b3fc426102dd8b572ccfa7446aa3495ebdb9d87b
justintam5 <jt.tam514@gmail.com>
2020-11-16 21:51:36 -0500

    Redid theta calculations in for loops
    Works for all valid forward matrices, although does not yet automate the process of choosing between possible solutions for the theta1-5

--- Lab2_InverseQ.m
+++ Lab2_InverseQ.m
@@ -1,79 +1,73 @@
-%---------------------End Effector Values---------------------------
+clear
+%\\\\\\\\\\\\\\\\\\\\\\\\\\\\\Forward Model\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+%---------------------Theta Values---------------------------
+theta1 = 90;
+theta2 = -20;
+theta3 = -80;
+theta4 = 19;
+theta5 = 0;
+delta = 90;
+%--------------------Calculate Q-------------------------------
+a = theta3+theta4+delta; %arbritrary constant for simplicity
+b = theta1+theta2;
+c = theta5;
+Q = [cosd(c)*cosd(b)*cosd(a)+sind(c)*sind(b) -sind(c)*cosd(b)*cosd(a)+cosd(c)*sind(b)...
+    cosd(b)*sind(a) cosd(b)*(110*sind(a)+60*cosd(theta3)+96)+98*cosd(theta1);
+    cosd(c)*sind(b)*cosd(a)-sind(c)*cosd(b) -sind(c)*sind(b)*cosd(a)-cosd(c)*cosd(b)...
+    sind(b)*sind(a) sind(b)*(110*sind(a)+60*cosd(theta3)+96)+98*sind(theta1);
+    cosd(theta5)*sind(a) -sind(theta5)*sind(a) -cosd(a) -110*cosd(a)+60*sind(theta3)+157;
+    0 0 0 1];
+%disp(Theta);
+%\\\\\\\\\\\\\\\\\\\\\\\\\\\\\Inverse Model\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+%---------------------End Effector Values----------------------------------
 %{
 Q=[0 0.7071 -0.7071 -118.7939;
     0 0.7071 0.7071 257.3869;
   1.0000 0 0 157.0000;
   0 0 0 1];
+delta=90;
 %}
-%thetai=0;
-%phii=-90;
-%psii=180;
-%delta=90;
+%--------------------Define Output Matrix----------------------------------
+%Using the given equations we expect 8 mathematically possible solutions.  
+%To organize this, we create a 8x10 matrix where the rows represent 
+%seperate solutions,and the columns represent various values that are 
+%either ouputs, values helpful in calculations, or indicators of nonviable 
+%solutions. These are defined as such:
+%\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+%Columns: |theta1|theta2|theta3|theta4|theta5|gamma|A|B|C|rt|
+%         |      |      |      |      |      |     | | | |  |
+%         |      |      |      |      |      |     | | | |  |
+%         |      |      |      |      |      |     | | | |  |...
+%\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+%where A,B,C, and gamma are used in calculations, and rt represents the
+%terms being squarerooted during the calculation of theta3
+EE=zeros(8,10);
 %--------------------Calculate thetai-------------------------------
 U=98*Q(2,3);
 V=-98*Q(1,3);
 W=Q(1,4)*Q(2,3)-Q(2,4)*Q(1,3);
-theta5i=atan2d(-Q(3,2),Q(3,1)); %one solution for theta5i
-theta1ia=2*atan2d((-V+sqrt(V^2+(U+W)*(U-W))),(-U-W));
-theta1ib=2*atan2d((-V-sqrt(V^2+(U+W)*(U-W))),(-U-W));
-theta2ia=(atan2d(Q(2,3),Q(1,3))-theta1ia);
-theta2ib=(atan2d(Q(2,3),Q(1,3))-theta1ib);
-%for 2 solutions of theta1/2, we get 2 solutions for gamma, and thus 2
-%solutions for our A,B, and C constants used to calculate theta3
-gammaa=(Q(1,4)-98*cosd(theta1ia))/(cosd(theta1ia+theta2ia));
-gammab=(Q(1,4)-98*cosd(theta1ib))/(cosd(theta1ib+theta2ib));
-Aa=11520-120*gammaa; Ab=11520-120*gammab;
-Ba=18840-120*Q(3,4); Bb=Ba;
-Ca=-(gammaa^2)+192*gammaa-(Q(3,4))^2+314*Q(3,4)-25365;
-Cb=-(gammab^2)+192*gammab-(Q(3,4))^2+314*Q(3,4)-25365;
-%Using theta1/2 to calculate theta3, we obtain 4 solutions for theta3
-a = Ba^2+(Aa+Ca)*(Aa-Ca);
-b = Bb^2+(Ab+Cb)*(Ab-Cb);
-theta3ia=0;
-theta3ib=0;
-theta3ic=0;
-theta3id=0;
-if a>=0
-    theta3ia=2*atan2d((-Ba+sqrt(Ba^2+(Aa+Ca)*(Aa-Ca))),(-Aa-Ca));
-    theta3ic=2*atan2d((-Ba-sqrt(Ba^2+(Aa+Ca)*(Aa-Ca))),(-Aa-Ca));
-end 
-if b>=0
-    theta3ib=2*atan2d((-Bb+sqrt(Bb^2+(Ab+Cb)*(Ab-Cb))),(-Ab-Cb));
-    theta3id=2*atan2d((-Bb-sqrt(Bb^2+(Ab+Cb)*(Ab-Cb))),(-Ab-Cb));
+%Calculate theta1, notice we get 2 possible solutions, denoted as s1 and s2
+for i=1:8
+    j=fix((i+1)/2);
+    k=fix((i+3)/4);
+    EE(i,1)=2*atan2d((-V+((-1)^i)*sqrt(V^2+(U+W)*(U-W))),(-U-W)); %theta1
+    EE(i,2)=atan2d(((-1)^j)*Q(2,3),((-1)^j)*Q(1,3))-EE(i,1); %theta2
+    %Where sind(theta1+theta2) != 90 Deg +-180 for gamma:
+    EE(i,6)=(Q(1,4)-98*cosd(EE(i,1)))/(cosd(EE(i,1)+EE(i,2))); %gamma 
+    EE(i,7)=11520-120*EE(i,6); %A
+    EE(i,8)=18840-120*Q(3,4); %B
+    EE(i,9)=-(EE(i,6)^2)+192*EE(i,6)-(Q(3,4))^2+314*Q(3,4)-25365; %C
+    EE(i,10)=EE(i,8)^2+(EE(i,7)+EE(i,9))*(EE(i,7)-EE(i,9)); %rt
+    if EE(i,10)>=0
+        EE(i,3)=2*atan2d((-EE(i,8)+((-1)^k)*sqrt(EE(i,10))),...
+            (-EE(i,7)-EE(i,9)));
+    end
+    %Where sind(theta1+theta2) != 90 Deg +-180 for theta4 calculated below:
+    EE(i,4)=atan2d(Q(1,3)/cosd(EE(i,1)+EE(i,2)),(-Q(3,3)))-EE(i,3)-delta;
+    EE(i,5)=atan2d(-Q(3,2)/(sind(EE(i,3)+EE(i,4)+delta)),...
+        Q(3,1)/(sind(EE(i,3)+EE(i,4)+delta)));
 end
-%Option 1 of calculating theta4:
-%{
-theta4ia=acosd(-Q(3,3))-theta3ia-delta;
-theta4ib=-theta4ia;
-theta4ic=acosd(-Q(3,3))-theta3ia-delta;
-%}
-%Option 2 of calculating theta4:
 %
-%We then compute 4 values for theta4, choosing which equation to use based
-%off of a condition placed on theta 1 and 2. The condition is calculated
-%for both sets of theta1/2 values
-alpha=theta1ia+theta2ia;
-beta=theta1ib+theta2ib;
-%For the theta1ia and theta2ia set:
-if a>=0 && alpha~=0 && alpha~=180 && alpha~=-180
-    theta4ia=atan2d((Q(2,3))/sind(alpha),-Q(3,3))-theta3ia-delta;
-    theta4ic=atan2d((Q(2,3))/sind(alpha),-Q(3,3))-theta3ic-delta;
-elseif a>=0
-    theta4ia=atan2d((Q(1,3))/cosd(alpha),-Q(3,3))-theta3ia-delta;
-    theta4ic=atan2d((Q(1,3))/cosd(alpha),-Q(3,3))-theta3ic-delta;
-end
-
-%For the theta1ib and theta2ib set:
-if b>=0 && beta~=0 && beta~=180 && beta~=-180
-    theta4id=atan2d((Q(2,3))/sind(beta),-Q(3,3))-theta3ib-delta;
-    theta4ib=atan2d((Q(2,3))/sind(beta),-Q(3,3))-theta3id-delta;
-elseif b>=0
-    theta4ib=atan2d((Q(1,3))/cosd(beta),-Q(3,3))-theta3ib-delta;
-    theta4id=atan2d((Q(1,3))/cosd(beta),-Q(3,3))-theta3id-delta;
-end
-
-%}
 %----------Final end effector location and angles--------------
-
-%--------------------Display possible soutions of thetai_matrix------------
-%thetai_matrix=[theta1i;theta2i;theta3i;theta4i;theta5i;]
+disp('| Theta1  | Theta2  | Theta3  | Theta4  | Theta5 |')
+disp(EE(:,1:5));
